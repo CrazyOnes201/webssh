66f9da26a32c9e06f5a5006f4551ac2d153ffdc4
CrazyOnes <crazyones201@163.com>
2018-01-07 21:05:03 +0800

    删除测试类，并对部分文件进行精简

--- src/main/java/com/liu/dao/TestDAO.java
+++
@@ -1,8 +0,0 @@
-package com.liu.dao;
-
-/**
- * Created by Crazy Ones on 2017/12/29.
- */
-public interface TestDAO {
-    public void insert(int num);
-}
--- src/main/java/com/liu/util/OperateString.java
+++ src/main/java/com/liu/util/OperateString.java
@@ -4,6 +4,7 @@ package com.liu.util;
  * Created by Crazy Ones on 2018/1/7.
  */
 public class OperateString {
+
     public static boolean stringIsEmpty(String s) {
         if(s == null || "".equals(s)) {
             return true;
